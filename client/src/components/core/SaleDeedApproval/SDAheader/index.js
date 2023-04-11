2ec8192b3e85fa9eb70d7ecfa08acf37931d1117
Sriraj Shirodkar <srirajshirodkar27969@gmail.com>
2023-04-11 19:53:58 +0530

    sale deed approval page creation

---
+++ client/src/components/core/SaleDeedApproval/SDAheader/index.js
@@ -0,0 +1,21 @@
+import { styled } from '@mui/material';
+
+const Header = styled("div")({
+    fontSize: '3rem',
+    // padding: '0.3rem',
+});
+
+const User = styled("div")({
+    marginBottom: '1rem'
+})
+
+const SaleDeedHeader = () => {
+  return (
+    <>
+        <User>HELLO JOHN DOE</User>
+        <Header>Sale Deed Creation</Header>
+    </>
+  )
+}
+
+export default SaleDeedHeader
