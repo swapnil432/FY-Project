6ec45e399f7f17f11cea5da346a10106f6b7bc79
Swapnil <satardekarss2002@gmail.com>
2023-04-16 21:09:10 +0530

    admin backend

--- server/server.js
+++ server/server.js
@@ -29,3 +29,32 @@ mongoose
   .catch((err) => console.log("Connection Error", err));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const Admin = require("./models/admin");
+
+async function seedUser() {
+  const username = "Admin";
+  const public_key = "0xdd47fab7ad68bdbb1993ac1f2be7998a700561ac";
+  const email = "admin@uri.com";
+  const password = "password";
+
+  const existingUser = await Admin.findOne({ email });
+
+  if (existingUser) {
+    console.log("User already exists in the database");
+    return;
+  }
+
+  const admin = new Admin({
+    username,
+    public_key,
+    email,
+    password,
+  });
+
+  await admin.hashPassword();
+  await admin.save();
+
+  console.log("User created successfully");
+}
+
+seedUser();
