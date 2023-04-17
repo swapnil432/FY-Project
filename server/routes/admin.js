6b48b41895d4c8088f8ba744806c3ebb385645ab
Swapnil <satardekarss2002@gmail.com>
2023-04-17 17:43:06 +0530

    admin api added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { login } from "../controllers/admin";
+import {
+  getPropertyDocuments,
+  getPropertyImages,
+  login,
+} from "../controllers/admin";
 const router = express.Router();
 
 router.post("/adminlogin", login);
+router.get("/getpropertydocs/:id", getPropertyDocuments);
+router.get("/getpropertyimages/:id", getPropertyImages);
 
 module.exports = router;
