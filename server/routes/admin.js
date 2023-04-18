6d2ffb312c747d299c7ade1e9e66b5c7e969cba7
Swapnil <satardekarss2002@gmail.com>
2023-04-18 17:46:26 +0530

    admin routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,11 +2,25 @@ import express from "express";
 import {
   getPropertyDocuments,
   getPropertyImages,
+  verifyProperty,
+  statusApprove,
+  statusReject,
+  getAllUsers,
+  getCompleteUsers,
+  verifyUser,
+  getAllProperty,
   login,
 } from "../controllers/admin";
 const router = express.Router();
 
 router.post("/adminlogin", login);
+router.get("/getallproperties", getAllProperty);
+router.get("/getallusers", getAllUsers);
+router.get("/getcompleteusers", getCompleteUsers);
+router.get("/verifyproperty/:id", verifyProperty);
+router.get("/verifyuser/:id", verifyUser);
+router.get("/statusApprove/:id", statusApprove);
+router.get("/statusreject/:id", statusReject);
 router.get("/getpropertydocs/:id", getPropertyDocuments);
 router.get("/getpropertyimages/:id", getPropertyImages);
 
